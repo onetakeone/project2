69a30bb8f750d81b0ac2389d1758f8d64f078cb3
N <limepassion@gmail.com>
2017-08-10 11:43:26 +0300

    refactoring: app contr comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # Loads devise before any action
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters  
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+
+    # Creates objects for devise user profile page. Objects are used to show user's posts and places  
     @places = Place.all
     @posts = Post.all
   end
+  
 
 
 end
