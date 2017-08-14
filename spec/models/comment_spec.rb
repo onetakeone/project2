696424eed4f7dc6c41e140fa8d582fc6e1ba9562
N <limepassion@gmail.com>
2017-08-14 11:15:04 +0300

    RSPEC: Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,7 @@
-require "spec_helper"
 require "rails_helper"
 
 describe Comment do
-  it { should validate_presence_of :comment }
+  it { should validate_presence_of(:content) }
+  it { should validate_length_of(:content).is_at_most(4000) }  
+  it { should belong_to(:commentable) }
 end
