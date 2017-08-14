14ae4b41e79297574ccc088f30b6cec7a155dbbd
N <limepassion@gmail.com>
2017-08-14 13:29:42 +0300

    validation: Username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   has_many :places, dependent: :destroy
   has_many :posts, dependent: :destroy
   devise :database_authenticatable, :registerable,
