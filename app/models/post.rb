9f70fed7fa4a3ced0262d964e089350f2dfcd817
N <limepassion@gmail.com>
2017-08-14 13:46:20 +0300

    Validations: Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 150}
+  validates :body, presence: true, length: { maximum: 4000}
+
   has_many :comments, as: :commentable
   belongs_to :user
+
+  def random_test
+    title
+  end
 end
