aa08bea1ae044e7e229b033fab1943b9e2e6d915
N <limepassion@gmail.com>
2017-09-12 00:12:59 +0300

    capybara test refactor

--- spec/features/user_create_comment_spec.rb
+++ spec/features/user_create_comment_spec.rb
@@ -3,21 +3,11 @@ include Warden::Test::Helpers
 
 feature "access check content create" do
   
-  scenario "access and check" do
-    #login via Warden helper login_as
-    visit '/users/sign_in/'  
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user, :run_callbacks => false)
+  scenario "access and check" do    
+    sign_in      
     visit '/places/'  
-    #Internalisation check
-    expect(page).to have_content I18n.t('places.places')   
-  end 
-
-  scenario "create" do
-    visit '/users/sign_in/'  
-      user = FactoryGirl.create(:user)
-      login_as(user, :scope => :user, :run_callbacks => false)
- 
+      #Internalisation check
+      expect(page).to have_content I18n.t('places.places')
     visit '/places/new'  
       fill_in 'Title', with: 'Title-test'
       fill_in 'Description', with: 'Description-test'
@@ -26,5 +16,11 @@ feature "access check content create" do
       click_button 'Settle place'
       expect(page).to have_content 'created'  
   end 
-    
+end
+
+#sign in via Warden helper login_as
+def sign_in 
+  visit new_user_session_path  
+  user = FactoryGirl.create(:user)
+  login_as(user, :scope => :user, :run_callbacks => false)
 end
