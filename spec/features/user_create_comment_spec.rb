27ce7617cd63be681ed43faaa3c42e25f0bbe881
N <limepassion@gmail.com>
2017-09-11 16:23:52 +0300

    capybara test (check content, fill, click)

--- spec/features/user_create_comment_spec.rb
+++ spec/features/user_create_comment_spec.rb
@@ -1,14 +1,30 @@
 require "rails_helper"
 include Warden::Test::Helpers
 
-feature "comment creation" do
+feature "access check content create" do
   
-  scenario "access" do
+  scenario "access and check" do
+    #login via Warden helper login_as
     visit '/users/sign_in/'  
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user, :run_callbacks => false)
     visit '/places/'  
-    expect(page).to have_content 'Places'    
+    #Internalisation check
+    expect(page).to have_content I18n.t('places.places')   
   end 
-  
+
+  scenario "create" do
+    visit '/users/sign_in/'  
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user, :run_callbacks => false)
+ 
+    visit '/places/new'  
+      fill_in 'Title', with: 'Title-test'
+      fill_in 'Description', with: 'Description-test'
+      fill_in 'Address', with: 'Address-test'
+      fill_in 'Tags', with: 'Tags'
+      click_button 'Settle place'
+      expect(page).to have_content 'created'  
+  end 
+    
 end
